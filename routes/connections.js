9a984f593e58c1ed8c25abf659f41aec754af395
jinkins <nick-1138@hotmail.com>
2018-01-12 08:41:41 +0100

    connections done

--- routes/connections.js
+++ routes/connections.js
@@ -55,6 +55,87 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.post('/search', function (req, res, next) {
+
+    let sql = 'SELECT * FROM connections' // where lifnr = + mysql.escape(req.params.id);
+    let i = 0;
+
+    if (req.body.tp) {
+        if (i == 0) {
+            sql = sql + " WHERE tp LIKE '%" + req.body.tp + "%'";
+            i++;
+        } else {
+            sql = sql + " AND tp LIKE '%" + req.body.tp + "%'";
+            i++;
+        }
+    }
+
+    if (req.body.message) {
+        if (i == 0) {
+            sql = sql + " WHERE message LIKE '%" + req.body.message + "%'";
+            i++;
+        } else {
+            sql = sql + " AND message LIKE '%" + req.body.message + "%'";
+            i++;
+        }
+    }
+
+    if (req.body.copy) {
+        if (i == 0) {
+            sql = sql + " WHERE copy LIKE '%" + req.body.copy + "%'";
+            i++;
+        } else {
+            sql = sql + " AND copy LIKE '%" + req.body.copy + "%'";
+            i++;
+        }
+    }
+
+    if (req.body.gln) {
+        if (i == 0) {
+            sql = sql + " WHERE gln = " + mysql.escape(req.body.gln);
+            i++;
+        } else {
+            sql = sql + " AND gln = " + mysql.escape(req.body.gln);
+            i++;
+        }
+    }
+    console.log(sql);
+
+
+    db.connection.getConnection(function (errCon, con) {
+        if (errCon) {
+            console.log(errCon);
+            return res.status(500).json({
+                status: 'KO',
+                reason: 'La connexion a la base de données a échouée.',
+                error: errCon
+            })
+        } else {
+            db.connection.query(sql, function (errData, results, fields) {
+                if (errData) {
+                    con.release();
+                    console.log(errData);
+                    return res.status(500).json({
+                        status: 'KO',
+                        reason: 'Erreur lors de la récupération des données',
+                        error: errData
+
+                    })
+                } else {
+                    con.release();
+                    if (results.length == 0) {
+                        return res.status(200).json([{}]) // Return an empty array due to no results     
+
+                    } else {
+                        return res.status(200).json(results);
+                    }
+
+                }
+            })
+        }
+    });
+})
+
 
 
 module.exports = router;
